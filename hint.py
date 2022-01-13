f9cd494db75302b4f1e08135c93724e600a0edac
Myrat Nesirov <myratnesirov@gmail.com>
2022-01-13 11:32:59 -0600

    Task1

---
+++ hint.py
@@ -0,0 +1,22 @@
+# her sorag üçin KÖMEKÇILER
+"""
+1. # çykmaly netije (sample output):
+
+  ['Aman', 'Berdi', 'Yslam', 'Myrat']
+
+2. # kömek (hint):
+
+  naharlaryň atlaryny key we string, bahalary value we integer edip beriň
+
+3. # kömek (hint):
+  for loop ulanyp ATLAR listindäki elementleri bir-birden input-yň içinde alyp soraň we naharlar listine append ediň
+
+4. # kömek (hint):
+
+  NAHARLAR listiny loop edip, her elementini (nahary) MENU dictionary-daky key edip beriň (Şonda size her naharyň bahasyny berer). We täze jemi_baha üýtgeýjisini alyp ilki nola deňläň, we loop-yň içinde her naharyň bahasyny üstüne goşuň.
+
+5. # çykmaly netije (sample output):
+
+    plany plança manat tölemeli!
+    
+"""
---
+++ main.py
@@ -0,0 +1,36 @@
+# Günüň soragy:
+
+"""
+1. Aman, Berdi, Yslam, Myrat 4 kişi kafe gitdiler. 4 kişilik list dörediň we ATLAR diýen üýtgeýjä (variable) deňläň.
+"""
+# 1. kody aşakda ýazyň:
+
+
+"""
+2. Dictionary ulanyp aşakdaky menu-y düzüň we MENU atly üýtgeýjä (variable) beriň:
+
+  Merjimek => 10$
+  Palaw => 15$
+  Işlekli => 20$
+  Gazanlama => 25$
+"""
+# 2. kody aşakda ýazyň:
+
+
+"""
+3. Aljak naharlaryny input arkaly soraň. Her kişi üçin bir nahar alyň we täze NAHARLAR listinde jemläň
+"""
+# 3. kody aşakda ýazyň:
+
+
+"""
+4. Ähli saýlanan naharlaryň jemi bahasyny çykaryň
+"""
+# 4. kody aşakda ýazyň:
+
+
+"""
+5. Kimiň tölejegini random ulanyp saýlaň
+"""
+# 5. kody aşakda ýazyň:
+
